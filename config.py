3307dae4623a474e2d29e1d276cb3abbc4b9cee1
saloni <138708065+saloni0318@users.noreply.github.com>
2026-04-19 15:21:09 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+# config_example.py — copy to config.py and fill with real values.
+NEO4J_URI = "neo4j+s://4********3.databases.neo4j.io"
+NEO4J_USER = "neo4j"
+NEO4J_PASSWORD = "nyLeYmjoT9*********EkItmiZ1ZtYLIxpuSE"
+
+HF_TOKEN = "hf_eDExC***********8pHUFWTrb" #huggingface (model:google/embeddinggemma-300m)
+PINECONE_API_KEY = "pcsk_2NECtE**************oXgta8wi2x"
+PINECONE_ENV = "us-east-1"   
+PINECONE_INDEX_NAME = "vietnam-travel"
+PINECONE_VECTOR_DIM = 768   
+
+HF_EMBED_MODEL = "google/embeddinggemma-300m"
+LLM_MODEL = "meta-llama/Llama-3.1-8b-instruct"
+TOP_K = 5
+MAX_TOKENS = 1000
